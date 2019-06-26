3a8a308baebaa50bafe77380a684a50f303fa27a
josernesto <jecm3>
2019-06-26 15:06:49 -0400

    commit con el login page creado, el instancelocator y el mainviewmodel

---
+++ Appf/Appf/Appf/App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Appf.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace Appf
+{
+    public partial class App : Application
+    {
+        #region Constructor
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new LoginPage();
+        }
+        #endregion
+
+        #region Methods
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+        #endregion
+    }
+}
